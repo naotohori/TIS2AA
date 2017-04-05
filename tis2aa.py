7f60e29ba73b2f0d97c53d614f6ed7fc7e260b05
naotohori <hori.naoto@gmail.com>
2017-04-04 19:52:17 -0500

    WIP tis2aa.py almost done as the first viersion.

--- tis2aa.py
+++ tis2aa.py
@@ -7,17 +7,21 @@ LIBPDBCG = 'RNA09_FRAG_CG/'  # output
 LISTFILE = 'RNA09.nts'
 
 PSEUDO_LIMIT = 2.5   ## +/- 2.5  ===>  bins of 5 degree
+PSEUDO_MAX = 10.0
 RMSD_ACCEPT = 2.0
+RMSD_MAX = 6.0
 
 from cafysis.elements.pdb import Chain, Residue, Atom
 from cafysis.file_io.pdb import PdbFile
 from cafysis.torsion import torsion
+from cafysis.mtx_coord_transform import mtx_crd_transform
 from CalcROT import calcrotation
 import sys
 import numpy as np
+import copy
 
-#p = PdbFile(sys.argv[1])
-p = PdbFile('f024_run019_2000.pdb')
+p = PdbFile(sys.argv[1])
+#p = PdbFile('f024_run019_2000.pdb')
 p.open_to_read()
 chains_cg = p.read_all()
 p.close()
@@ -40,8 +44,9 @@ def angle_diff(ang1, ang2):
         d = d - 180.0
     return d
 
+chains_aa = []
 for c_cg in chains_cg:
-
+        
     seq = []
     xyzs_P = []
     xyzs_S = []
@@ -92,10 +97,20 @@ for c_cg in chains_cg:
         theta  = torsion(xyzP2, xyzS2, xyzP3, xyzS3, flg_degree=True, flg_360=True)
         
 
-        limit = PSEUDO_LIMIT
-        
+        """ Search best fragment """
+        ilimit = 0
         flg_found = False
         while not flg_found:
+
+            ilimit += 1
+            limit = PSEUDO_LIMIT * ilimit
+
+            if limit > PSEUDO_MAX:
+                if best_rmsd <= RMSD_MAX:
+                    break
+                else:
+                    print 'limit > PSEUDO_MAX; could not find library for ir=',ir
+                    sys.exit(2)
 
             lib_cand = []
             angle_cand = []
@@ -111,7 +126,7 @@ for c_cg in chains_cg:
     
             best_lib = 0
             best_rmsd = 999999.9
-            best_mtx = None
+            best_mat = None
             best_angle = None
             
             for lib, angle in zip(lib_cand, angle_cand):
@@ -124,21 +139,56 @@ for c_cg in chains_cg:
                             c.residues[1].atoms[2].xyz.get_as_list(), # B2
                             c.residues[2].atoms[0].xyz.get_as_list()] # P3
     
-                rmsd, mtx = calcrotation(np.transpose(xyzPSBP_ref), np.transpose(xyzPSBP))
+                rmsd, mat = calcrotation(np.transpose(xyzPSBP_ref), np.transpose(xyzPSBP))
     
                 if rmsd < best_rmsd:
                     best_rmsd = rmsd
                     best_lib = lib
-                    best_mtx = mtx
+                    best_mat = mat
                     best_angle = angle
     
                 f_log.write("#%3i %06i %f  %f %f\n" % (ir, lib, rmsd, angle[0], angle[1]))
 
             if best_rmsd <= RMSD_ACCEPT:
                 flg_found = True
-            else:
-                limit = limit + PSEUDO_LIMIT
 
         f_log.write('%3i %6i %f  %f %f\n' % (ir, len(lib_cand), best_rmsd, best_angle[0], best_angle[1]))
 
+        p = PdbFile('%s/%06i_%s.pdb' % (LIBPDBAA, best_lib, seq[ir]))
+        p.open_to_read()
+        frag_res = p.read_all()[0].residues[1]
+        p.close()
+
+        mtx = mtx_crd_transform()
+        mtx.mtx[:,:] = best_mat
+
+        r_aa = Residue()
+        for a in frag_res.atoms:
+            a_new = copy.deepcopy(a)
+            a_new.xyz.put_as_list( mtx.do_to_array( a.xyz.get_as_list() ) )
+            r_aa.push_atom( a_new )
+
+        c_aa.push_residue( r_aa )
+
+    chains_aa.append( c_aa )
+
+
+serial = 0
+res_seq = 0
+for c in chains_aa:
+    for r in c.residues:
+        res_seq += 1
+        for a in r.atoms:
+            serial += 1
+            a.serial = serial
+            a.res_seq = res_seq
+    c.reindex()
+
+p = PdbFile(sys.argv[-1])
+#p = PdbFile('tis2aa.pdb')
+p.open_to_write()
+p.write_all( chains_aa )
+p.close()
+
 f_log.close()
+

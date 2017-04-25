06a3d80a0e4907f36c2298a90a810e552be8afce
naotohori <hori.naoto@gmail.com>
2017-04-24 19:00:40 -0500

    Minor updates

--- min.py
+++ min.py
@@ -1,42 +1,41 @@
 #!/usr/bin/env python
+''' Wrapper of tleap & sander '''
+__author__ = "Naoto Hori"
 
 import subprocess
 import argparse
 import shutil
 
-parser = argparse.ArgumentParser(description='Minimize given structure using sander (jsut a wrapper for sander!)')
+parser = argparse.ArgumentParser(description='Minimize given structure using sander (jsut a wrapper for sander!)',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('--noH', dest='flg_noH', default=False,
-                    action='store_true', help='Hydrogen atoms are removed from output')
-parser.add_argument('--rest', dest='restraint', action='store',
-                             default=50.0, help='restraint_wt in sander') 
-parser.add_argument('--ncyc', dest='ncyc', action='store',
-                             default=200, help='ncyc in sander') 
-parser.add_argument('--maxcyc', dest='maxcyc', action='store',
-                             default=500, help='maxcyc in sander') 
-parser.add_argument('--prefix', dest='prefix', action='store',
-                            default='min', help='prefix for output files')
+                    action='store_true', help='Remove hydrogen atoms from output')
+parser.add_argument('--rest', dest='restraint', default=50.0,
+                    action='store', type=float, help='[sander parameter] restraint_wt') 
+parser.add_argument('--maxcyc', dest='maxcyc', default=500,
+                    action='store', type=int, help='[sander parameter] maxcyc') 
+parser.add_argument('--ncyc', dest='ncyc', default=200,
+                    action='store', type=int, help='[sander parameter] ncyc') 
+parser.add_argument('--prefix', dest='prefix', default='min',
+                    action='store', help='prefix for sander output files')
 
-parser.add_argument('pdb_in', help='target PDB file')
+parser.add_argument('pdb_in',  help='target PDB file')
 parser.add_argument('pdb_out', help='output minimized PDB file')
 
 args = parser.parse_args()
 
-flg_noH = args.flg_noH
-restraint = args.restraint
-ncyc = args.ncyc
-maxcyc = args.maxcyc
-prefix = args.prefix
+################################################################################
 
 file_pdb = args.pdb_in
 file_minpdb = args.pdb_out
 
-file_min = prefix + '.in'
-file_out = prefix + '.out'
-file_leap = prefix + '.leap.in'
-file_prmtop = prefix + '.prmtop'
-file_inpcrd = prefix + '.inpcrd'
-file_rst = prefix + '.rst'
+file_min = args.prefix + '.in'
+file_out = args.prefix + '.out'
+file_leap = args.prefix + '.leap.in'
+file_prmtop = args.prefix + '.prmtop'
+file_inpcrd = args.prefix + '.inpcrd'
+file_rst = args.prefix + '.rst'
 
 ################################################################################
 
@@ -61,11 +60,11 @@ f_out = open(file_min,'w')
 f_out.write("initial minimization whole system\n")
 f_out.write("&cntrl\n")
 f_out.write("  imin   = 1\n")
-f_out.write("  ncyc = %i, maxcyc = %i\n" % (ncyc, maxcyc))
+f_out.write("  ncyc = %i, maxcyc = %i\n" % (args.ncyc, args.maxcyc))
 f_out.write("  ntb    = 0\n")
 f_out.write("  cut    = 999.0\n")
 f_out.write("  ntr = 1\n")
-f_out.write("  restraint_wt = %f\n" % restraint)
+f_out.write("  restraint_wt = %f\n" % args.restraint)
 f_out.write('  restraintmask="@P"\n')
 f_out.write("/\n")
 f_out.close()
@@ -86,7 +85,7 @@ f_minpdb.close()
 '''
 Remove hydrogens (optional)
 '''
-if flg_noH:
+if args.flg_noH:
 
     pdb = open(file_minpdb+'.noH','w') 
 

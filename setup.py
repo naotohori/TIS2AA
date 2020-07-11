12002af5aff7c137cc3c78bb4add3ef5ac487811
Naoto Hori <hori.naoto@gmail.com>
2020-07-11 16:30:35 +0100

    Initiated setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="TIS2AA",
+    version='2.0',
+    description='Reconstruction of atomistic structure from coarse-grained DNA/RNA model',
+    author='Naoto Hori',
+    author_email='hori.naoto@gmail.com',
+    url='https://github.com/naotohori/TIS2AA',
+)
